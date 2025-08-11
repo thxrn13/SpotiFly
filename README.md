843a6b1c189c08aababb45314bb94a2522541f93
thxrn13 <wrp9493@gmail.com>
2025-08-11 17:08:45 -0400

    added spotify connnection

--- README.md
+++ README.md
@@ -1,18 +1,39 @@
 # Spotifyplayer app
 
+## Set up an app in Spotify's developer dashboard
+
+1) Go to [Spotify's Developer Site](https://developer.spotify.com)
+2) Log in
+3) Navigate to: _your profile_ >> _Dashboard_ >> _Create app_
+4) App name can be whatever you want (e.g. ```SpotiFly```)
+5) List any description you want, this part is not important
+6) Redirect URI: ```http://127.0.0.1:6969/oauth_callback```
+7) Create app and copy `Client ID` and `Client secret`
+
+## Set environment variables
+
+Place Client ID and Client secret into an ```.env``` file in the root directory with the structure:
+
+``` markdown
+CLIENT_ID = <Client Id from Spotify>
+CLIENT_SECRET = <Client Secret from Spotify>
+```
+
+An [example](.env_example) is provided for ease of use.
+
 ## Run the app
 
 ### uv
 
 Run as a desktop app:
 
-```
+``` terminal
 uv run flet run
 ```
 
 Run as a web app:
 
-```
+``` terminal
 uv run flet run --web
 ```
 
@@ -20,19 +41,19 @@ uv run flet run --web
 
 Install dependencies from `pyproject.toml`:
 
-```
+``` terminal
 poetry install
 ```
 
 Run as a desktop app:
 
-```
+``` terminal
 poetry run flet run
 ```
 
 Run as a web app:
 
-```
+``` terminal
 poetry run flet run --web
 ```
 
@@ -42,7 +63,7 @@ For more details on running the app, refer to the [Getting Started Guide](https:
 
 ### Android
 
-```
+``` terminal
 flet build apk -v
 ```
 
@@ -50,7 +71,7 @@ For more details on building and signing `.apk` or `.aab`, refer to the [Android
 
 ### iOS
 
-```
+``` terminal
 flet build ipa -v
 ```
 
@@ -58,7 +79,7 @@ For more details on building and signing `.ipa`, refer to the [iOS Packaging Gui
 
 ### macOS
 
-```
+``` terminal
 flet build macos -v
 ```
 
@@ -66,7 +87,7 @@ For more details on building macOS package, refer to the [macOS Packaging Guide]
 
 ### Linux
 
-```
+``` terminal
 flet build linux -v
 ```
 
@@ -74,7 +95,7 @@ For more details on building Linux package, refer to the [Linux Packaging Guide]
 
 ### Windows
 
-```
+``` terminal
 flet build windows -v
 ```
 
--- src/main.py
+++ src/main.py
@@ -1,14 +1,59 @@
+import os
 import flet as ft
 import flet_audio as fa
+from flet.auth import OAuthProvider
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
+PORT = 6969
+REDIRECT_URI = f"http://127.0.0.1:{PORT}/oauth_callback"
+SCOPE = ["user-read-private",
+         "user-read-email",
+         "user-library-read",
+         "user-read-recently-played",
+         "user-top-read",
+         "user-read-playback-state",
+         "user-modify-playback-state",
+         "user-read-currently-playing",
+         "playlist-read-collaborative",
+         "playlist-modify-public",
+         "playlist-modify-private",
+         "playlist-read-public",
+         "playlist-read-private",
+         "playlist-modify-public",
+         "streaming",
+         "app-remote-control",
+         "user-personalized"]
+AUTH_URL = "https://accounts.spotify.com/authorize"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
 url = "https://github.com/mdn/webaudio-examples/blob/main/audio-basics/outfoxing.mp3?raw=true"
 
 
 def main(page: ft.Page):
+    provider = OAuthProvider(
+        client_id=CLIENT_ID,
+        authorization_endpoint=AUTH_URL,
+        token_endpoint=TOKEN_URL,
+        redirect_url=REDIRECT_URI,
+        scopes=SCOPE,
+        client_secret=CLIENT_SECRET,
+        user_endpoint="https://api.spotify.com/v1/me"
+    )
+
+    def login_click(e):
+        page.login(provider)
+
+    def on_login(e):
+        print("Login error:", e.error)
+        print("Access token:", page.auth.token)
+
+    page.on_login = on_login
+
     def change_play_pause_button(e):
-        # print(e.data)
-        # print(e.data == ft.AudioState.PLAYING.value)
         if e.data == ft.AudioState.PAUSED.value:
             play_pause_button.icon = ft.Icons.PLAY_ARROW
             play_pause_button.on_click = lambda e: audio1.resume()
@@ -37,32 +82,45 @@ def main(page: ft.Page):
     )
 
     page.overlay.append(audio1)
+
     page.add(
-        ft.BottomAppBar(
-            padding=3,
-            content=ft.Column(
-                spacing=3,
-                alignment=ft.MainAxisAlignment.CENTER,
-                controls=[
-                    ft.Row(
+        ft.Column(
+            controls=[
+                ft.Button(
+                    text="Authorize with Spotify",
+                    on_click=lambda e: login_click(e),
+                    bgcolor="green",
+                    color="white",
+                    width=200,
+                    height=50,
+                ),
+                ft.BottomAppBar(
+                    padding=3,
+                    content=ft.Column(
+                        spacing=3,
                         alignment=ft.MainAxisAlignment.CENTER,
                         controls=[
-                            ft.Text("LOREM IPSUM")
-                        ]
-                    ),
-                    ft.Row(
-                        alignment=ft.MainAxisAlignment.CENTER,
-                        vertical_alignment=ft.CrossAxisAlignment.CENTER,
-                        controls=[
-                            ft.IconButton(icon=ft.Icons.SKIP_PREVIOUS, icon_color="white"),
-                            play_pause_button,
-                            ft.IconButton(icon=ft.Icons.SKIP_NEXT, icon_color="white")
+                            ft.Row(
+                                alignment=ft.MainAxisAlignment.CENTER,
+                                controls=[
+                                    ft.Text("LOREM IPSUM")
+                                ]
+                            ),
+                            ft.Row(
+                                alignment=ft.MainAxisAlignment.CENTER,
+                                vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                                controls=[
+                                    ft.IconButton(icon=ft.Icons.SKIP_PREVIOUS, icon_color="white"),
+                                    play_pause_button,
+                                    ft.IconButton(icon=ft.Icons.SKIP_NEXT, icon_color="white")
+                                ],
+                            ),
                         ],
-                    ),
-                ],
-            )
+                    )
+                ),
+            ]
         )
     )
 
 
-ft.app(main)
+ft.app(main, port=PORT, view=ft.WEB_BROWSER)
