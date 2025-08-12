43839e88a064b206305d13b19a3c8b2933e47f74
thxrn <wrp9493@gmail.com>
2025-08-12 12:44:09 -0400

    add ability to log out

--- src/main.py
+++ src/main.py
@@ -20,10 +20,10 @@ config.read('.config')
 PORT = config.getint('General', 'PORT')
 AUTH_URL = config.get('General', 'AUTH_URL')
 TOKEN_URL = config.get('General', 'TOKEN_URL')
-SCOPE = json.loads(config.get('General', 'SCOPE'))
 USER_ENDPOINT = config.get('General', 'USER_ENDPOINT')
 REDIRECT_URI = f"http://127.0.0.1:{PORT}/oauth_callback"
-url = "https://github.com/mdn/webaudio-examples/blob/main/audio-basics/outfoxing.mp3?raw=true"
+url = ("https://github.com/"
+       "mdn/webaudio-examples/blob/main/audio-basics/outfoxing.mp3?raw=true")
 
 # Set up Security
 if not os.getenv("SECRET_KEY"):
@@ -54,7 +54,7 @@ def main(page: ft.Page):
             user_resp = httpx.get(USER_ENDPOINT, headers=headers)
             user_resp.raise_for_status()
             user_info = user_resp.json()
-            logged_in_user_info.value = f"Logged in as: {
+            logged_user.value = f"Logged in as: {
                 user_info.get('display_name', 'Unknown')}"
             page.update()
 
@@ -73,10 +73,8 @@ def main(page: ft.Page):
         jt = page.auth.token.to_json()  # type: ignore
         ejt = encrypt(jt, key)  # type: ignore
         page.client_storage.set("myapp.auth_token", ejt)
-
+        toggle_login_button()
         get_user_info()
-
-    page.on_login = on_login
 
     def change_play_pause_button(e):
         if e.data == ft.AudioState.PAUSED.value:
@@ -94,6 +92,40 @@ def main(page: ft.Page):
         on_click=lambda _: audio1.play()
         )
 
+    def logout_button_click(e):
+        page.client_storage.remove("myapp.auth_token")
+        page.logout()
+
+    def on_logout(e):
+        toggle_login_button()
+        page.update()
+
+    def toggle_login_button():
+        login_button.visible = page.auth is None
+        logged_user.visible = logout_button.visible = page.auth is not None
+        page.update()
+
+    page.on_login = on_login
+    page.on_logout = on_logout
+    login_button = ft.ElevatedButton(
+        text="Login with Spotify",
+        on_click=perform_login,
+        bgcolor="green",
+        color="white",
+        width=200,
+        height=50,
+    )
+
+    logout_button = ft.ElevatedButton(
+        text="Logout",
+        on_click=logout_button_click,
+        bgcolor="red",
+        color="white",
+        width=200,
+        height=50,
+        visible=False,
+    )
+
     audio1 = fa.Audio(
         src=url,
         autoplay=False,
@@ -108,22 +140,16 @@ def main(page: ft.Page):
 
     page.overlay.append(audio1)
 
-    logged_in_user_info = ft.Text()
+    logged_user = ft.Text()
 
     page.add(
         ft.Column(
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             controls=[
-                ft.Button(
-                    text="Authorize with Spotify",
-                    on_click=lambda e: perform_login(e),
-                    bgcolor="green",
-                    color="white",
-                    width=200,
-                    height=50,
-                ),
-                logged_in_user_info,
+                login_button,
+                logout_button,
+                logged_user,
                 ft.BottomAppBar(
                     padding=3,
                     content=ft.Column(
@@ -138,15 +164,18 @@ def main(page: ft.Page):
                             ),
                             ft.Row(
                                 alignment=ft.MainAxisAlignment.CENTER,
-                                vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                                vertical_alignment=ft.CrossAxisAlignment
+                                                     .CENTER,
                                 controls=[
                                     ft.IconButton(
                                         icon=ft.Icons.SKIP_PREVIOUS,
-                                        icon_color="white"),
+                                        icon_color="white"
+                                    ),
                                     play_pause_button,
                                     ft.IconButton(
                                         icon=ft.Icons.SKIP_NEXT,
-                                        icon_color="white")
+                                        icon_color="white"
+                                    )
                                 ],
                             ),
                         ],
